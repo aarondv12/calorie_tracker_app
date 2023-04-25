412d8e732ed9e750541f09dfca7d73542c697f44
sherifelnady <sherifelnady4@gmail.com>
2023-04-25 17:52:45 +0100

    changed number

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Calorie tracker app 6'),
+      home: const MyHomePage(title: 'Calorie tracker app 7'),
     );
   }
 }
