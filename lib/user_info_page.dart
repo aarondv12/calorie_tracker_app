c0b787b0eda3e01977c7e1a00bfaed3d21fbdf25
markjup2062838 <up2062838@myport.ac.uk>
2023-05-10 19:10:08 +0100

    User Info Page

---
+++ lib/user_info_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'calorietrackpage.dart';
+
+class UserInfoPage extends StatefulWidget {
+  @override
+  _UserInfoPageState createState() => _UserInfoPageState();
+}
