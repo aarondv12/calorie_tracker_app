c7b4de756a144060682cdb378d542d5b53e442a8
sherifelnady <sherifelnady4@gmail.com>
2023-05-10 19:16:20 +0100

    create a history page class

--- lib/HistoryPage.dart
+++ lib/HistoryPage.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'hive_helper.dart';
 import 'food_item.dart';
+
+class HistoryPage extends StatefulWidget {
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
