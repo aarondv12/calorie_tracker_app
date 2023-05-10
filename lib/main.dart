1d21cffffd846f670889feb2c6f28e061b01a57b
aaronvarghese <vargheseaaron20@gmail.com>
2023-05-10 14:32:39 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,5 +14,44 @@ void main() async {
 }
 
 int calculateDailyCalories(int age, double height, double weight, String gender, String goal) {
-  // function content here
+  double bmr;
+
+  if (gender == 'Male') {
+    bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+  } else {
+    bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+  }
+
+  double multiplier;
+
+  switch (goal) {
+    case 'Lose weight':
+      multiplier = 1.2;
+      break;
+    case 'Maintain weight':
+      multiplier = 1.55;
+      break;
+    case 'Gain weight':
+      multiplier = 1.9;
+      break;
+    default:
+      multiplier = 1.55;
+  }
+
+  int dailyCalories = (bmr * multiplier).round();
+
+  return dailyCalories;
+}
+}
+class CalorieTrackerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calorie Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StartPage(),
+    );
+  }
 }
