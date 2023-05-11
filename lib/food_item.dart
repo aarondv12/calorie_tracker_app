f197d3e556abb0d06181ff61b537e2edeefd8aaa
markjup2062838 <up2062838@myport.ac.uk>
2023-05-11 08:51:18 +0100

    Clarifying the Food Items

--- lib/food_item.dart
+++ lib/food_item.dart
@@ -2,10 +2,18 @@ import 'package:hive/hive.dart';
 
 part 'food_item.g.dart';
 
+/// A [FoodItem] class representing an individual food item with its name and calories.
+///
+/// This class is annotated with `@HiveType` to enable Hive support for serialization.
+/// The `typeId` is set to 0 to uniquely identify the class within Hive.
 @HiveType(typeId: 0)
 class FoodItem extends HiveObject {
+  /// The name of the food item.
+  ///
+  /// Annotated with `@HiveField(0)` for Hive support.
   @HiveField(0)
   String name;
+
 
   @HiveField(1)
   int calories;
